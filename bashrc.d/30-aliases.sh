1d5567a7ef5ab7068e3a0aa19b5b4da3436839ba
Gerard van Helden <gerard@zicht.nl>
2016-08-15 10:14:45 +0200

    stash

--- bashrc.d/30-aliases.sh
+++ bashrc.d/30-aliases.sh
@@ -1,6 +1,6 @@
 alias x=startx
 alias z=$HOME/work/zicht/z-installer/vendor/bin/z
-
+alias c="php app/console"
 function missing_trans()
 {
     tail -400 app/logs/development.log  | grep -i 'translation not found' | awk -F ":" '{ print $5 }' | awk -F "," '{ print $1 }'
